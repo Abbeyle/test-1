b4dddf23fa235aae4e9d05da6a23d338e5c30f20
abbeyle <ayomideoloye425@gmail.com>
2022-12-17 07:30:48 -0800

    learning how to write function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //adding numbers in javascript
   
-  var a = 5
-  var b = 5
-
-console.log(alert( + a + b ));
+  function add(num1,num2) {
+     return num1 + num2    
+  };
  
+ function subtract(num1,num2) {
+  return num1 - num2    
+};
+
+function multiply(num1,num2) {
+  return num1 * num2    
+};
+
+console.log ("addition of your numbers is " + add(2,3));
+console.log ("subtraction of your numbers is " + subtract(2,3));
+console.log ("multiplication of your numbers is  " + multiply(2,3));
