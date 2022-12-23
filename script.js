f14970a425a8b175003dba996389243f1652cf29
abbeyle <ayomideoloye425@gmail.com>
2022-12-23 10:35:30 -0800

    modify

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //adding numbers in javascript
-  
-  function add(num1,num2) {
-     return num1 + num2    
-  };
- 
- function subtract(num1,num2) {
-  return num1 - num2    
+//addition of numbers
+let a = prompt("what is your age")
+let b = prompt("what is your partner's age")
+//addition of a and b
+
+function add(num1,num2){
+  return(a + b)
 };
 
-function multiply(num1,num2) {
-  return num1 * num2    
+function subtract(num1,num2){
+  return(a - b)
 };
 
-console.log ("addition of your numbers is " + add(2,3));
-console.log ("subtraction of your numbers is " + subtract(2,3));
-console.log ("multiplication of your numbers is  " + multiply(2,3));
+function multiply(num1,num2){
+  return(a * b)
+};
+
+alert("addition of your age and your partner's is " + add(a + b));
+alert("subtraction of your age and your partner's is " + subtract(a - b));
+alert("multiplication of your age and your partner's is " + multiply(a * b));
